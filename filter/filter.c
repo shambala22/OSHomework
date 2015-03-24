f40297d2775d0e616238b7779e97ecec4eecb6ab
shambala22 <asmavrin@rain.ifmo.ru>
2015-03-25 02:12:48 +0300

    helpers done

---
+++ filter/filter.c
@@ -0,0 +1,5 @@
+//
+// Created by shambala on 25.03.15.
+//
+
+#include "filter.h"
---
+++ filter/filter.h
@@ -0,0 +1,8 @@
+//
+// Created by shambala on 25.03.15.
+//
+
+#ifndef _OSHW3_FILTER_H_
+#define _OSHW3_FILTER_H_
+
+#endif //_OSHW3_FILTER_H_
