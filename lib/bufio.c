54fc39d8ce6f770cadcd4f2c2be5b0f4e936c9f8
shambala22 <asmavrin@rain.ifmo.ru>
2015-04-03 04:18:49 +0300

    lib

--- lib/bufio.c
+++ lib/bufio.c
@@ -2,6 +2,8 @@
 // Created by shambala on 03.04.15.
 //
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "bufio.h"
 
 #ifdef DEBUG
@@ -43,3 +45,40 @@ size_t buf_size(struct buf *b) {
     return b->size;
 }
 
+ssize_t buf_fill(int fd, struct buf *b, size_t req) {
+    abort(buf == NULL || req > b->capacity);
+    int res = 0;
+    int count = req - b->size;
+    while(1) {
+        res = read(fd, b->data + b->size, b->capacity - b->size);
+        //RETHROW_IO(res);
+        if (res >= count) {
+            return b->size += res;
+        }
+        if (res == 0) {
+            return b->size;
+        }
+        b->size += res;
+        count -= res;
+    }
+}
+
+ssize_t buf_flush(int fd, struct buf *b, size_t req) {
+    abort(b==NULL || req > b->capacity);
+    int res = 0;
+    int count = (int) req;
+    int wr = 0;
+    for(;;) {
+        res = (int) write(fd, b->data + wr, b->size - wr);
+        if (res==-1) break;
+        wr += res;
+        if (res>=count) break;
+        if (write==b->size) break;
+        count -= res;
+    }
+    if ((b->size-=wr)>0) {
+        memmove(b, b+wr, b->size);
+    }
+    RETHROW_IO(res);
+    return wr;
+}
